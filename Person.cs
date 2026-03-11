fd321e2b441f7b9ed62e96c9b9dd4bef5eea0821
Brendon W. <bwherry@midland.k12.ia.us>
2026-03-11 15:39:57 +0000

    gg3

--- Person.cs
+++ Person.cs
@@ -1,13 +1,32 @@
 ﻿namespace knightmoves;
 
-public class Person{
-   public string FirstName;
-   public string LastName;
-   public string[] Hobbies;
+public class Person
+{
+    public string FirstName;
+    public string LastName;
+    public string[] Hobbies;
 
-   public Person(string firstName, string lastName, params string[] AddHobbies){
-      FirstName = firstName;
-      LastName = lastName;
-      Hobbies = AddHobbies;
-   }
+    public Person(string firstName, string lastName, params string[] addHobbies)
+    {
+        FirstName = firstName;
+        LastName = lastName;
+        Hobbies = addHobbies;
+    }
+
+    public void AddHobbies(params string[] newHobbies)
+    {
+        string[] combined = new string[Hobbies.Length + newHobbies.Length];
+
+        for (int i = 0; i < Hobbies.Length; i++)
+        {
+            combined[i] = Hobbies[i];
+        }
+
+        for (int i = 0; i < newHobbies.Length; i++)
+        {
+            combined[Hobbies.Length + i] = newHobbies[i];
+        }
+
+        Hobbies = combined;
+    }
 }
